96a1b2a5d8f61d7e312a830c6c8af7f71bb84c41
talal <talalbadreddine@hotmail.com>
2022-07-01 23:51:48 +0300

    configure all the router path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import NavBar from "./Components/NavBar";
+import NavBar from "./Routes/Navigation/NavBar";
+import { Route,Routes } from "react-router-dom";
+import Home from "./Routes/Home/Home";
+import SignIn from "./Routes/SignIn/SignIn";
+import Shop from "./Routes/Shop/Shop";
+import Contact from "./Routes/ContactUs/ContactUs";
+import Payments from "./Routes/Payments/Payments";
 import './index.css'
 
 function App() {
   return (
-    <div className="">
-      <NavBar></NavBar>
+    <div>
+      <NavBar/>
+
+      <Routes>
+          <Route path="/" element = {<Home />}></Route>
+          <Route path="/SignIn" element = {<SignIn />}></Route>
+          <Route path="/Shop" element = {<Shop />}></Route>
+          <Route path="/Contact" element = {<Contact/>}></Route>
+          <Route path="/Payments" element = {<Payments />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Routes/SignIn/SignIn.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SignIn = () => {
+    return(
+        <h1>I am Sign In page</h1>
+    )
+}
+
+export default SignIn
