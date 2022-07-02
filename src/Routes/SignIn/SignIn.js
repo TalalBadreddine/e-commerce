ac80c2ca3e364aa933698a4cbfb4a50a4f655c52
talal <talalbadreddine@hotmail.com>
2022-07-02 22:31:57 +0300

    added sign in with google

--- src/Routes/SignIn/SignIn.js
+++ src/Routes/SignIn/SignIn.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { signInWithGooglePopUp, createUserDocFromAuth } from "../../Utils/FireBase/FireBaseUtils";
 
 const SignIn = () => {
+
+    const logInWithGoogle = async () => {
+        const response = await signInWithGooglePopUp()
+        createUserDocFromAuth(response.user)
+    }
+
     return(
-        <h1>I am Sign In page</h1>
+        <>
+            <h1>I am Sign In page</h1>
+             <button onClick={logInWithGoogle}>
+               Sign In
+            </button>
+        </>
     )
 }
 
---
+++ src/Utils/FireBase/FireBaseUtils.js
@@ -0,0 +1,50 @@
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signInWithRedirect  } from "firebase/auth";
+import {getFirestore, doc , getDoc, setDoc} from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "e-commerce-e05e4.firebaseapp.com",
+  projectId: "e-commerce-e05e4",
+  storageBucket: "e-commerce-e05e4.appspot.com",
+  messagingSenderId: "413487506580",
+  appId: "1:413487506580:web:3e235561b32173f0a34081",
+  measurementId: "G-Z9FLFCDPVX"
+};
+
+const firebaseApp = initializeApp(firebaseConfig)
+const provider = new GoogleAuthProvider()
+
+provider.setCustomParameters({
+    prompt:'select_account'
+})
+
+export const auth = getAuth(firebaseApp);
+export const signInWithGooglePopUp = () => signInWithPopup(auth, provider)
+export const db = getFirestore()
+
+export const createUserDocFromAuth =  async (userInfo) => {
+    const userDocument = doc(db, 'users', userInfo.uid)
+    const snapShot = await getDoc(userDocument)
+    const userExist = await snapShot.exists()
+    console.log(userInfo)
+
+    if(!userExist){
+        try{
+
+            await setDoc(userDocument, {
+                name: userInfo.displayName,
+                email: userInfo.email,
+                registerAt: new Date()
+            })
+
+        }
+        catch(err){
+            console.log('error creating user ', err.message)
+        }
+
+
+    }
+
+
+}
